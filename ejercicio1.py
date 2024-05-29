76bfcf02abe50272dd5614487b25a06936eb3da1
Giovdev <gio.orellanac@duocuc.cl>
2024-05-29 10:58:02 -0400

    Desarrollo 2

---
+++ ejercicio1.py
@@ -0,0 +1,30 @@
+import os
+import msvcrt
+import time
+
+usuarios = []
+
+while True:
+    print("""
+    1. Inicio sesión
+    2. Registrar usuario
+    3. Eliminar usuario
+    4 Salir""")
+
+    opc = int(input("opción elegida: "))
+
+    if opc==1:
+        pass
+    elif opc==2:
+        print("Registrar usuario")
+        nombre = input("Nombre: ")
+        contrasena = input("Contraseña: ")
+        usuario = {
+            "Nombre": nombre,
+            "Contrasena": contrasena,}
+        usuarios.append(usuario)
+    elif opc==3:
+        pass
+    else:
+        print("ADIOS!")
+        time.sleep(2)
